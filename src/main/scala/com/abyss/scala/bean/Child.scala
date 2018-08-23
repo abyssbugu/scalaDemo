715be74570dd504e982c728f99839bf74fc7be0d
Abyss <547161555@qq.com>
2018-08-23 20:16:32 +0800

    继承

---
+++ src/main/scala/com/abyss/scala/bean/Child.scala
@@ -0,0 +1,15 @@
+package com.abyss.scala.bean
+
+/**
+  * Created by Abyss on 2018/8/23.
+  * description:
+  */
+class Child extends Father {
+  //继承加上关键字
+  override val name: String = "sub"
+
+  //子类可以定义自己的 field 和 method
+  val score = "A"
+
+  def getScore = this.score
+}
---
+++ src/main/scala/com/abyss/scala/bean/Father.scala
@@ -0,0 +1,11 @@
+package com.abyss.scala.bean
+
+/**
+  * Created by Abyss on 2018/8/23.
+  * description:
+  */
+class Father {
+  val name = "super"
+
+  def getName = this.name
+}
