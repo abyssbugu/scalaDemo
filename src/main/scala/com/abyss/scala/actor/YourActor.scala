ead7b5ee254c644fde5aec2fdb2a4f3b70b1b619
Abyss <547161555@qq.com>
2018-08-24 16:51:59 +0800

    用react替代receive接受消息

---
+++ src/main/scala/com/abyss/scala/actor/YourActor.scala
@@ -0,0 +1,37 @@
+package com.abyss.scala.actor
+
+import scala.actors.Actor
+
+/**
+  * Created by Abyss on 2018/8/24.
+  * ! 发送异步消息,没有返回值
+  * !? 发送同步消息,等待返回值
+  * !! 发送异步消息,返回值是Future[Any]
+  *
+  */
+class YourActor extends Actor {
+  override def act(): Unit = {
+    loop {
+      react {
+        case "start" => println("starting")
+        case "stop" => println("stoping")
+      }
+    }
+
+
+  }
+}
+
+object YourActor {
+  def main(args: Array[String]): Unit = {
+    val actor = new YourActor()
+    actor.start()
+    actor ! "start"
+    actor ! "stop"
+    println("发送消息成功")
+  }
+}
+
+
+
+
